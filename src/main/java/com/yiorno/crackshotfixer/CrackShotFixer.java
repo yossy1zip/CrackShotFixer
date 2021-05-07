a328a4ef8a9fcf0032a857f4b7d2500849559013
yossy_zip <mattsu.zip@gmail.com>
2021-05-07 20:27:06 +0900

    add shulkerpacks blocker (on a inventory)

--- src/main/java/com/yiorno/crackshotfixer/CrackShotFixer.java
+++ src/main/java/com/yiorno/crackshotfixer/CrackShotFixer.java
@@ -7,7 +7,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
@@ -118,6 +121,72 @@ public final class CrackShotFixer extends JavaPlugin implements Listener {
                 return;
             }
 
+        }
+
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onInvClick(InventoryClickEvent e){
+        Player player = (Player) e.getWhoClicked();
+
+        ItemStack heldItem = e.getCurrentItem();
+        Material heldMaterial = heldItem.getType();
+
+        shuklerBoxes.add(Material.SHULKER_BOX);
+        shuklerBoxes.add(Material.SHULKER_BOX);
+        shuklerBoxes.add(Material.WHITE_SHULKER_BOX);
+        shuklerBoxes.add(Material.ORANGE_SHULKER_BOX);
+        shuklerBoxes.add(Material.MAGENTA_SHULKER_BOX);
+        shuklerBoxes.add(Material.LIGHT_BLUE_SHULKER_BOX);
+        shuklerBoxes.add(Material.YELLOW_SHULKER_BOX);
+        shuklerBoxes.add(Material.LIME_SHULKER_BOX);
+        shuklerBoxes.add(Material.PINK_SHULKER_BOX);
+        shuklerBoxes.add(Material.GRAY_SHULKER_BOX);
+        shuklerBoxes.add(Material.LIGHT_GRAY_SHULKER_BOX);
+        shuklerBoxes.add(Material.CYAN_SHULKER_BOX);
+        shuklerBoxes.add(Material.PURPLE_SHULKER_BOX);
+        shuklerBoxes.add(Material.BLUE_SHULKER_BOX);
+        shuklerBoxes.add(Material.BROWN_SHULKER_BOX);
+        shuklerBoxes.add(Material.GREEN_SHULKER_BOX);
+        shuklerBoxes.add(Material.RED_SHULKER_BOX);
+        shuklerBoxes.add(Material.BLACK_SHULKER_BOX);
+
+
+        if(reloadingPlayer.contains(player)) {
+
+
+            if (shuklerBoxes.contains(heldMaterial)) {
+
+                if (e.getClick() != ClickType.RIGHT) {
+
+                    return;
+
+                }
+
+                e.setCancelled(true);
+
+                if (!coolingPlayer.contains(player)) {
+
+                    player.sendMessage(ChatColor.YELLOW + "リロード中はシュルカーボックスを開けません；；");
+                    coolingPlayer.add(player);
+
+                    BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+                    scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                        @Override
+                        public void run() {
+                            coolingPlayer.remove(player);
+                        }
+                    }, 20L);
+
+                    return;
+
+                } else {
+                    return;
+                }
+
+            } else {
+                return;
+            }
         }
 
     }
