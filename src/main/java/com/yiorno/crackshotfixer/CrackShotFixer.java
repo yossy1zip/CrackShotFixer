073a9cb1f1c78ab31b912179621844aa587d4f85
yossy_zip <mattsu.zip@gmail.com>
2021-05-09 01:57:16 +0900

    add detection of a slot change

--- src/main/java/com/yiorno/crackshotfixer/CrackShotFixer.java
+++ src/main/java/com/yiorno/crackshotfixer/CrackShotFixer.java
@@ -30,10 +30,6 @@ public final class CrackShotFixer extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        saveDefaultConfig();
-        Config config = new Config(this);
-        config.load();
-
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -47,89 +43,87 @@ public final class CrackShotFixer extends JavaPlugin implements Listener {
         Player player = e.getPlayer();
         reloadingPlayer.add(player);
 
-        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
-        scheduler.scheduleSyncDelayedTask(this, new Runnable() {
-            @Override
-            public void run() {
-                reloadingPlayer.remove(player);
-            }
-        }, 100L);
+        return;
 
     }
-
-    //@EventHandler
-    //public void onReloadComplete(WeaponReloadCompleteEvent e) {
-    //    Player player = e.getPlayer();
-    //    reloadingPlayer.remove(player);
-    //    player.sendMessage(ChatColor.YELLOW + "reloaded");
-    //}
 
     @EventHandler
-    public void onHeldItem(PlayerItemHeldEvent e){
+    public void onReloadComplete(WeaponReloadCompleteEvent e) {
+        Player player = e.getPlayer();
+        reloadingPlayer.remove(player);
+
+        return;
+    }
+
+    @EventHandler
+    public void onCancelReload(PlayerItemHeldEvent e){
 
         Player player = e.getPlayer();
-        int heldSlot = e.getNewSlot();
-        ItemStack heldItem = player.getInventory().getItem(heldSlot);
-        Material heldMaterial = heldItem.getType();
 
-        shuklerBoxes.add(Material.SHULKER_BOX);
-        shuklerBoxes.add(Material.SHULKER_BOX);
-        shuklerBoxes.add(Material.WHITE_SHULKER_BOX);
-        shuklerBoxes.add(Material.ORANGE_SHULKER_BOX);
-        shuklerBoxes.add(Material.MAGENTA_SHULKER_BOX);
-        shuklerBoxes.add(Material.LIGHT_BLUE_SHULKER_BOX);
-        shuklerBoxes.add(Material.YELLOW_SHULKER_BOX);
-        shuklerBoxes.add(Material.LIME_SHULKER_BOX);
-        shuklerBoxes.add(Material.PINK_SHULKER_BOX);
-        shuklerBoxes.add(Material.GRAY_SHULKER_BOX);
-        shuklerBoxes.add(Material.LIGHT_GRAY_SHULKER_BOX);
-        shuklerBoxes.add(Material.CYAN_SHULKER_BOX);
-        shuklerBoxes.add(Material.PURPLE_SHULKER_BOX);
-        shuklerBoxes.add(Material.BLUE_SHULKER_BOX);
-        shuklerBoxes.add(Material.BROWN_SHULKER_BOX);
-        shuklerBoxes.add(Material.GREEN_SHULKER_BOX);
-        shuklerBoxes.add(Material.RED_SHULKER_BOX);
-        shuklerBoxes.add(Material.BLACK_SHULKER_BOX);
-
-
-        if(reloadingPlayer.contains(player)){
-
-            if(shuklerBoxes.contains(heldMaterial)) {
-
-                e.setCancelled(true);
-
-                if (!coolingPlayer.contains(player)) {
-
-                    player.sendMessage(ChatColor.YELLOW + "リロード中はシュルカーボックスを持てません；；");
-                    coolingPlayer.add(player);
-
-                    BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
-                    scheduler.scheduleSyncDelayedTask(this, new Runnable() {
-                        @Override
-                        public void run() {
-                            coolingPlayer.remove(player);
-                        }
-                    }, 20L);
-
-                    return;
-
-                } else {
-                    return;
-                }
-
-            }else{
-                return;
-            }
-
+        if (!reloadingPlayer.contains(player)){
+            return;
         }
 
+        reloadingPlayer.remove(player);
+
+        //Material heldMaterial = player.getInventory().getItem(e.getNewSlot()).getType();
     }
+
+
+    //@EventHandler
+    //public void onHeldItem(PlayerItemHeldEvent e){
+
+    //    Player player = e.getPlayer();
+    //    int heldSlot = e.getNewSlot();
+    //    ItemStack heldItem = player.getInventory().getItem(heldSlot);
+    //    Material heldMaterial = heldItem.getType();
+
+
+
+
+    //    if(reloadingPlayer.contains(player)){
+
+    //        if(shuklerBoxes.contains(heldMaterial)) {
+
+    //            e.setCancelled(true);
+
+    //            if (!coolingPlayer.contains(player)) {
+
+    //                player.sendMessage(ChatColor.YELLOW + "リロード中はシュルカーボックスを持てません；；");
+    //                coolingPlayer.add(player);
+
+    //                BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+    //                scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+    //                    @Override
+    //                    public void run() {
+    //                        coolingPlayer.remove(player);
+    //                    }
+    //                }, 20L);
+
+    //                return;
+
+    //            } else {
+    //                return;
+    //            }
+
+    //        }else{
+    //            return;
+    //        }
+
+    //    }
+
+    //}
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onInvClick(InventoryClickEvent e){
         Player player = (Player) e.getWhoClicked();
 
         ItemStack heldItem = e.getCurrentItem();
+
+        if (heldItem == null){
+            return;
+        }
+
         Material heldMaterial = heldItem.getType();
 
         shuklerBoxes.add(Material.SHULKER_BOX);
